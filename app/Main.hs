4fd3ee96b99cfefd5ac8828863dcb54a77c47f41
Max Strübing <max_struebing@gmx.de>
2016-09-17 13:40:08 +0200

    task: add basic cli argument parsing implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,44 @@
 module Main where
 
+import Options.Applicative
 import Lib
+import Data.Char
+
+data TodoApp = TodoApp { 
+      todo :: Maybe String 
+    , show :: Bool
+    , delete :: Bool }
+
+todoApp :: Parser TodoApp
+todoApp = TodoApp
+    <$> (optional $ strOption 
+        (long "add"
+        <> short 'a'
+        <> metavar "<TODO>"
+        <> help "add todo"))
+    <*> switch 
+        (long "show"
+        <> short 's'
+        <> help "show todos")
+    <*> switch 
+        (long "delete-all"
+        <> short 'D'
+        <> help "delete all todos")
+
+run :: TodoApp -> IO ()
+run (TodoApp x False False) = addOrShowTodos x
+run (TodoApp _ True _) = printFile -- check if file available
+run (TodoApp _ False True) = createTodoFile -- ask if file should be deleted
 
 main :: IO ()
-main = do
-    fileAvailable <- todoFileAvailable
-    if fileAvailable then printFile else createTodoFile
+main = execParser opts >>= run
+    where 
+        opts = info (helper <*> todoApp)
+            (fullDesc
+            <> progDesc "Manage your todos"
+            <> header "A todolist application writte in Haskell")
+
+addOrShowTodos :: Maybe String -> IO ()
+addOrShowTodos Nothing = printFile
+addOrShowTodos (Just x) = addTodo $ x
 
